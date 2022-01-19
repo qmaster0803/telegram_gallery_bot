43d832e950082c28db7cdf3c061e2c0a5c974b3d
Dan K <qmaster080305@gmail.com>
2022-01-19 14:51:18 +0300

    Initial commit

---
+++ database_module.py
@@ -0,0 +1,5 @@
+import sqllite3
+
+try:
+	db_conn = sqllite3.connect("Library/bot_database.db")
+def check_user_id(id):
--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-print("Starting...")
 import cv2
 import os
+import telebot
 import photos_module
+import config_module
 
-print("Opening...")
-photos = [photos_module.open_photo("Library/1.jpg"), photos_module.open_photo("Library/2.jpg"), photos_module.open_photo("Library/3.jpg"), photos_module.open_photo("Library/4.jpg"), photos_module.open_photo("Library/5.jpg"), photos_module.open_photo("Library/6.jpg"), photos_module.open_photo("Library/7.jpg"), photos_module.open_photo("Library/8.jpg")]
 
-print("Working...")
-for i in range(len(photos)):
-	photos[i] = photos_module.crop_and_resize(photos[i], use_blackpage=True)
+bot = telebot.TeleBot(config_module.bot_token)
 
-cv2.imshow("test", photos_module.create_preview_table(photos))
-cv2.waitKey(0)
+@bot.message_handler
+#print("Opening...")
+#photos = [photos_module.open_photo("Library/1.jpg"), photos_module.open_photo("Library/2.jpg"), photos_module.open_photo("Library/3.jpg"), photos_module.open_photo("Library/4.jpg"), photos_module.open_photo("Library/5.jpg"), photos_module.open_photo("Library/6.jpg"), photos_module.open_photo("Library/7.jpg"), photos_module.open_photo("Library/8.jpg")]
+
+#print("Working...")
+#for i in range(len(photos)):
+#	photos[i] = photos_module.crop_and_resize(photos[i], use_blackpage=True)
+
+#cv2.imshow("test", photos_module.create_preview_table(photos))
+#cv2.waitKey(0)
