0d80e48671454ce7de1f1e2058f3f86d761f5bc7
Dan K <qmaster080305@gmail.com>
2022-03-11 00:12:04 +0300

    Update default_config_module.py

--- default_config_module.py
+++ default_config_module.py
@@ -1,3 +1,4 @@
-bot_token = "<-- FILL -->"
-main_db_path = "Library/bot_database.db"
-library_path = "Library/"
+bot_token      = "<-- FILL -->"
+main_db_path   = "Library/bot_database.db"
+library_path   = "Library/"
+downloads_path = "tmp/"
